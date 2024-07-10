5b8f31e7b66534df77dbe7acbe2b01808eeb05b0
Aditya Vinod Dahake <aditya_vd@pursuitsoftware.com>
2024-07-10 13:50:04 +0530

    Add POST /email/send route

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -1,5 +1,10 @@
+const sendEmail = (req, res) => {
+  res.send("POST on /email/send");
+};
+
 module.exports = {
   get: (req, res) => {
     res.send("This is get request from EmailController");
   },
+  post: sendEmail,
 };
--- routes/Email.js
+++ routes/Email.js
@@ -4,5 +4,6 @@ const controller = require("../controllers/EmailController");
 const router = express.Router();
 
 router.get("/", controller.get);
+router.post("/send", controller.post);
 
 module.exports = router;

7bf7139c1099d5752c6855fd50255f4b2e0c44f4
Abhishek <abhishek_sg@pursuitsoftware.com>
2024-07-11 11:18:56 +0530

    added frontend and refactored code.

--- controllers/EmailController.js
+++ controllers/EmailController.js
@@ -47,57 +47,53 @@ const processEmailQueue = async (job) => {
 
 emailQueue.process(processEmailQueue);
 
+const post = async (req, res) => {
+  const { from, to, subject, text } = req.body;
+  await sendNewEmail({ from, to, subject, text });
+  
+  console.log("Added to queue");
+
+  res.json({
+    message: "Email Sent",
+  });
+};
+
+const send_without_queue = async (req, res) => {
+  const { from, to, subject, text } = req.body;
+
+  // Create a test account for nodemailer (useful for tutorials and testing)
+  const testAccount = await nodemailer.createTestAccount();
+  const transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, 
+    auth: {
+      user: testAccount.user,
+      pass: testAccount.pass,
+    },
+    tls: {
+      rejectUnauthorized: false, 
+    },
+  });
+  console.log("Sending mail to %s", to);
+  let info = await transporter.sendMail({
+    from, 
+    to, 
+    subject, 
+    text, 
+    html: `<strong>${text}</strong>`,
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  res.json({
+    message: "Email Sent",
+    messageId: info.messageId,
+    previewURL: nodemailer.getTestMessageUrl(info),
+  });
+};
+
 module.exports = {
-  get: (req, res) => {
-    res.send("This is get request from EmailController");
-  },
-  post: async (req, res) => {
-    const { from, to, subject, text } = req.body;
-
-    await sendNewEmail({ from, to, subject, text });
-
-    console.log("Added to queue");
-
-    res.json({
-      message: "Email Sent",
-    });
-  },
-  send_without_queue: async (req, res) => {
-    const { from, to, subject, text } = req.body;
-
-    // Use a test account as this is a tutorial
-    const testAccount = await nodemailer.createTestAccount();
-
-    const transporter = nodemailer.createTransport({
-      host: "smtp.ethereal.email",
-      port: 587,
-      secure: false,
-      auth: {
-        user: testAccount.user,
-        pass: testAccount.pass,
-      },
-      tls: {
-        rejectUnauthorized: false,
-      },
-    });
-
-    console.log("Sending mail to %s", to);
-
-    let info = await transporter.sendMail({
-      from,
-      to,
-      subject,
-      text,
-      html: `<strong>${text}</strong>`,
-    });
-
-    console.log("Message sent: %s", info.messageId);
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-
-    res.json({
-      message: "Email Sent",
-      messageId: info.messageId,
-      previewURL: nodemailer.getTestMessageUrl(info),
-    });
-  },
+  post,
+  send_without_queue,
 };

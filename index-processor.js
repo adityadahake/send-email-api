ea28b0f417b194414422bd273e61d6a777d1109f
Aditya Vinod Dahake <aditya_vd@pursuitsoftware.com>
2024-07-14 21:08:20 +0530

    separate reciever and sender

--- index-processor.js
+++ index-processor.js
@@ -1,31 +1,15 @@
-const nodemailer = require("nodemailer");
 const Bull = require("bull");
+const nodemailer = require("nodemailer");
 
 const emailQueue = new Bull("email", {
   // use your redis connection URL
   redis: "redis://127.0.0.1:6379",
 });
 
-const sendNewEmail = async (email) => {
-  await emailQueue.add({ ...email });
-};
-
-const post = async (req, res) => {
-  const { from, to, subject, text } = req.body;
-  await sendNewEmail({ from, to, subject, text });
-
-  console.log("Added to queue");
-
-  res.json({
-    message: "Email Sent",
-  });
-};
-
-const send_without_queue = async (req, res) => {
-  const { from, to, subject, text } = req.body;
-
-  // Create a test account for nodemailer (useful for tutorials and testing)
+const processEmailQueue = async (job) => {
+  // Use a test account as this is a tutorial
   const testAccount = await nodemailer.createTestAccount();
+
   const transporter = nodemailer.createTransport({
     host: "smtp.ethereal.email",
     port: 587,
@@ -38,7 +22,11 @@ const send_without_queue = async (req, res) => {
       rejectUnauthorized: false,
     },
   });
+
+  const { from, to, subject, text } = job.data;
+
   console.log("Sending mail to %s", to);
+
   let info = await transporter.sendMail({
     from,
     to,
@@ -49,14 +37,8 @@ const send_without_queue = async (req, res) => {
 
   console.log("Message sent: %s", info.messageId);
   console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  res.json({
-    message: "Email Sent",
-    messageId: info.messageId,
-    previewURL: nodemailer.getTestMessageUrl(info),
-  });
+
+  return nodemailer.getTestMessageUrl(info);
 };
 
-module.exports = {
-  post,
-  send_without_queue,
-};
+emailQueue.process(processEmailQueue);
